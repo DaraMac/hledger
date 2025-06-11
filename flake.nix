183e778568447dcdba1c81f2e3d5ff01dd8cd6bb
dmac <DaraMac@users.noreply.github.com>
2025-06-11 18:54:49 +0100

    Add flake for devshell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+    description = "hledger development flake";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs, }:
+    let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+        devShells."x86_64-linux".default = with pkgs; mkShell {
+            packages = with pkgs; [
+                cabal-install
+                ghc
+                ghcid
+                gnum4
+                groff
+                haskellPackages.hasktags
+                haskellPackages.quickbench
+                haskellPackages.shelltestrunner
+                just
+                pandoc
+                stack
+                watchexec
+            ];
+
+            shellHook = ''
+                echo "Activated hledger dev environment"
+                exec zsh
+                '';
+        };
+    };
+}
